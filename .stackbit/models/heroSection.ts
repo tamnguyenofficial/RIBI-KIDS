78a191b64adf42a7f908bd207d30cad047a6a2b0
tamnguyenofficial <tamdesignviet@gmail.com>
2025-07-12 09:02:40 +0700

    Update heroSection.ts

--- .stackbit/models/heroSection.ts
+++ .stackbit/models/heroSection.ts
@@ -1,16 +1,6 @@
-import { type ModelExtension } from '@stackbit/types';
-import { commonFields } from './sectionCommon';
-
-export const heroSection: ModelExtension = {
-    name: 'heroSection',
-    fields: [...commonFields]
-};
-<section class="bg-pink-100 py-16 px-4">
-  <!-- nội dung của hero -->
-</section>
 export default {
   type: 'object',
   name: 'heroSection',
-  label: 'Trang chính', // đổi tại đây
+  label: 'Trang Chủ',
   ...
 }
